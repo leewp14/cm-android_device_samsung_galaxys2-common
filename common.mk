a0c6311ed701a9464d2b1532c31f9391a5772a92
bauner <bauner@gmx.de>
2015-07-31 07:40:50 +0200

    use custom dalvik.vm.heapxxxx values

--- common.mk
+++ common.mk
@@ -213,7 +213,18 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.sys.isUsbOtgEnabled=true
 
-$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+# $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+# $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+# Custom dalvik.vm values
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=6m \
+    dalvik.vm.heapgrowthlimit=64m \
+    dalvik.vm.heapsize=160m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=1m \
+    dalvik.vm.heapmaxfree=4m
+
 
 # Include exynos4 platform specific parts
 TARGET_HAL_PATH := hardware/samsung/exynos4/hal
