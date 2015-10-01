68e962f5de0b39c4dc1f80ce4df4fdac0ff1bda6
bauner <bauner@gmx.de>
2015-10-01 10:08:39 +0200

    fix mms
    added
    
    ro.telephony.mms_data_profile=5
    
    to fix mms

--- common.mk
+++ common.mk
@@ -144,7 +144,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungExynos4RIL \
     mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1 \
     ro.telephony.call_ring.multiple=false \
-    ro.telephony.call_ring.delay=2000
+    ro.telephony.call_ring.delay=2000 \
+	ro.telephony.mms_data_profile=5
 
 # Camera
 # PRODUCT_PROPERTY_OVERRIDES += \
