2b2dd95c3a321783f00016d836890f26d46e1648
bauner <bauner@gmx.de>
2016-03-16 09:45:35 +0100

    f2fs tools

--- common.mk
+++ common.mk
@@ -153,6 +153,11 @@ PRODUCT_PACKAGES += \
     setup_fs \
     static_busybox
 
+# F2FS Filesystem
+PRODUCT_PACKAGES += \
+    fsck.f2fs \
+    mkfs.f2fs
+
 # Live Wallpapers
 PRODUCT_PACKAGES += \
     Galaxy4 \
