c55dfedbb977694d576f66abc83f8901831c319c
bauner <bauner@gmx.de>
2016-05-03 12:02:28 +0200

    pegasus powerhal

--- common.mk
+++ common.mk
@@ -114,6 +114,10 @@ PRODUCT_PACKAGES += \
     libSEC_OMX_Venc \
     libOMX.SEC.M4V.Encoder
 
+# Power
+PRODUCT_PACKAGES += \
+    power.smdk4210
+
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
