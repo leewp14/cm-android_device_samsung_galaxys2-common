4890baa8f8abc69ceca3a2588d43473df2a2672b
bauner <bauner@gmx.de>
2016-12-18 09:10:42 +0100

    update DisplayColorCalibration.java

--- cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
+++ cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
@@ -33,22 +33,11 @@ public class DisplayColorCalibration {
     private static final String GREEN_FILE = "/sys/class/mdnie/mdnie/g_adj";
     private static final String BLUE_FILE = "/sys/class/mdnie/mdnie/b_adj";
 
-    private static final boolean sUseGPUMode;
-
     private static final int MIN = 1;
     private static final int MAX = 255;
 
-    private static final int[] sCurColors = new int[] { MAX, MAX, MAX };
-
-    static {
-        // We can also support GPU transform using RenderEngine. This is not
-        // preferred though, as it has a high power cost.
-        sUseGPUMode = !FileUtils.isFileWritable(RED_FILE) ||
-                SystemProperties.getBoolean("debug.livedisplay.force_gpu", false);
-    }
-
     public static boolean isSupported() {
-        // Always true, use GPU mode if no HW support
+        // Always true
         return true;
     }
 
@@ -65,93 +54,17 @@ public class DisplayColorCalibration {
     }
 
     public static String getCurColors()  {
-        if (!sUseGPUMode) {
-            return String.format("%s %s %s",
-                    FileUtils.readOneLine(RED_FILE),
-                    FileUtils.readOneLine(GREEN_FILE),
-                    FileUtils.readOneLine(BLUE_FILE));
-        }
-
-        return String.format("%d %d %d", sCurColors[0],
-                sCurColors[1], sCurColors[2]);
+        return String.format("%s %s %s",
+            FileUtils.readOneLine(RED_FILE),
+            FileUtils.readOneLine(GREEN_FILE),
+            FileUtils.readOneLine(BLUE_FILE));
     }
 
     public static boolean setColors(String colors) {
-        if (!sUseGPUMode) {
-            String[] array = colors.split(" ");
-            return FileUtils.writeLine(RED_FILE, array[0]) &&
-                FileUtils.writeLine(GREEN_FILE, array[1]) &&
-                FileUtils.writeLine(BLUE_FILE, array[2]);
-        }
-
-        float[] mat = toColorMatrix(colors);
-
-        // set to null if identity
-        if (mat == null ||
-                (mat[0] == 1.0f && mat[5] == 1.0f &&
-                 mat[10] == 1.0f && mat[15] == 1.0f)) {
-            return setColorTransform(null);
-        }
-        return setColorTransform(mat);
+        String[] array = colors.split(" ");
+        return FileUtils.writeLine(RED_FILE, array[0]) &&
+            FileUtils.writeLine(GREEN_FILE, array[1]) &&
+            FileUtils.writeLine(BLUE_FILE, array[2]);
     }
-
-    private static float[] toColorMatrix(String rgbString) {
-        String[] adj = rgbString == null ? null : rgbString.split(" ");
-
-        if (adj == null || adj.length != 3) {
-            return null;
-        }
-
-        float[] mat = new float[16];
-
-        // sanity check
-        for (int i = 0; i < 3; i++) {
-            int v = Integer.parseInt(adj[i]);
-
-            if (v >= MAX) {
-                v = MAX;
-            } else if (v < MIN) {
-                v = MIN;
-            }
-
-            mat[i * 5] = (float)v / (float)MAX;
-            sCurColors[i] = v;
-        }
-
-        mat[15] = 1.0f;
-        return mat;
-    }
-
-    /**
-     * Sets the surface flinger's color transformation as a 4x4 matrix. If the
-     * matrix is null, color transformations are disabled.
-     *
-     * @param m the float array that holds the transformation matrix, or null to
-     *            disable transformation
-     */
-    private static boolean setColorTransform(float[] m) {
-        try {
-            final IBinder flinger = ServiceManager.getService("SurfaceFlinger");
-            if (flinger != null) {
-                final Parcel data = Parcel.obtain();
-                data.writeInterfaceToken("android.ui.ISurfaceComposer");
-                if (m != null) {
-                    data.writeInt(1);
-                    for (int i = 0; i < 16; i++) {
-                        data.writeFloat(m[i]);
-                    }
-                } else {
-                    data.writeInt(0);
-                }
-                flinger.transact(1030, data, null, 0);
-                data.recycle();
-            }
-        } catch (RemoteException ex) {
-            Slog.e(TAG, "Failed to set color transform", ex);
-            return false;
-        }
-        return true;
-    }
-
 }
 
