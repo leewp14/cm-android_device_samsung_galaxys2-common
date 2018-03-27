f370ccef93dd1b5915a6ad19e45e42f6765ef0c1
bauner <bauner@gmx.de>
2018-03-27 17:06:13 +0200

    build camera2

--- common.mk
+++ common.mk
@@ -115,7 +115,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     camera.device@1.0-impl-legacy \
     camera.smdk4210 \
-    Snap \
+    Camera2 \
     libstagefright_shim
 
 PRODUCT_COPY_FILES += \
