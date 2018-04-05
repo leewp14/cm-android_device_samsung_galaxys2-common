7499f0d79961fc26772d98cee72441c7fb72d610
bauner <bauner@gmx.de>
2018-04-05 08:27:23 +0200

    libwvm_shim: move to vendor

--- shims/libwvm_shim/Android.mk
+++ shims/libwvm_shim/Android.mk
@@ -6,5 +6,6 @@ LOCAL_SRC_FILES := wvm_shim.cpp
 LOCAL_SHARED_LIBRARIES := libstagefright_foundation
 LOCAL_MODULE := libwvm_shim
 LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
